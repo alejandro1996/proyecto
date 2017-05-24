37b2e381642fa6d7ca58569cfb0ce6babe5f15b5
alumno <alumno@HUEB4.labhue.local>
2017-05-23 19:37:30 -0600

    Las vistas

---
+++ src/main.java
@@ -0,0 +1,16 @@
+import Vista.*;
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JFprincipal frmprincipal = new JFprincipal();
+        frmprincipal.setExtendedState(frmprincipal.MAXIMIZED_HORIZ);
+        
+        frmprincipal.setVisible(true);
+        
+    }
+    
+}
